e960b8a1c5208f71e93560ca86760a8f55d64075
wadabender <alexshuseiwada@gmail.com>
2018-09-21 13:27:54 -0700

    changed index.js

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-// const http = require('http');
-// const { Client } = require('pg');
+const http = require('http');
+const { Client } = require('pg');
 const express = require('express');
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
-// const { DATABASE_URL } = process.env;
-// const server = http.createServer((req, res) => {
-//   const client = new Client({
-//     connectionString: DATABASE_URL,
-//   });
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   client.connect()
-//     .then(() => client.query('SELECT * FROM hellotable'))
-//     .then((result) => {
-//       res.end(`${result.rows[0].name}\n`);
-//       client.end();
-//     })
-//     .catch(() => {
-//       res.end('ERROR');
-//       client.end();
-//     });
-// });
+const PORT = process.env.PORT || 5000;
+const { DATABASE_URL } = process.env;
 
 app.set('view engine', 'ejs');
 
@@ -31,10 +14,31 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
   // eslint-disable-next-line
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
 
-// server.listen(PORT, () => {
-//   // eslint-disable-next-line
-//   console.log(`Server running on ${PORT}/`);
-// });
+const server = http.createServer((req, res) => {
+  const client = new Client({
+    connectionString: DATABASE_URL,
+  });
+  res.statusCode = 200;
+
+  client.connect();
+
+  // res.setHeader('Content-Type', 'text/plain');
+  // client.connect()
+  //   .then(() => client.query('SELECT * FROM hellotable'))
+  //   .then((result) => {
+  //     res.end(`${result.rows[0].name}\n`);
+  //     client.end();
+  //   })
+  //   .catch(() => {
+  //     res.end('ERROR');
+  //     client.end();
+  //   });
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line
+  console.log(`Server running on ${PORT}/`);
+});
