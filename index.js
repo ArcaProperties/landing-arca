a18d7ea7d74dbc5658c00e06748af53dcf19e955
wadabender <alexshuseiwada@gmail.com>
2018-09-21 13:12:37 -0700

    wooooo

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const http = require('http');
-const { Client } = require('pg');
+// const http = require('http');
+// const { Client } = require('pg');
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-const { DATABASE_URL } = process.env;
-const server = http.createServer((req, res) => {
-  const client = new Client({
-    connectionString: DATABASE_URL,
-  });
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  client.connect()
-    .then(() => client.query('SELECT * FROM hellotable'))
-    .then((result) => {
-      res.end(`${result.rows[0].name}\n`);
-      client.end();
-    })
-    .catch(() => {
-      res.end('ERROR');
-      client.end();
-    });
-});
+// const PORT = process.env.PORT || 5000;
+// const { DATABASE_URL } = process.env;
+// const server = http.createServer((req, res) => {
+//   const client = new Client({
+//     connectionString: DATABASE_URL,
+//   });
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   client.connect()
+//     .then(() => client.query('SELECT * FROM hellotable'))
+//     .then((result) => {
+//       res.end(`${result.rows[0].name}\n`);
+//       client.end();
+//     })
+//     .catch(() => {
+//       res.end('ERROR');
+//       client.end();
+//     });
+// });
 
 app.set('view engine', 'ejs');
 
@@ -34,7 +34,7 @@ app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
 
-server.listen(PORT, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on ${PORT}/`);
-});
+// server.listen(PORT, () => {
+//   // eslint-disable-next-line
+//   console.log(`Server running on ${PORT}/`);
+// });
